2665974459f914cc5aa54f354c4a162ae1b9b807
wangliansheng <8863704@qq.com>
2018-01-24 20:56:22 +0800

    router

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var app = express();
+var express=require('express');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var app = new express();
+app.get('/',function(req,res){
+    res.send('this is / page');
 });
+app.get('/login',function(req,res){
+    res.send('thisi is login');
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
+app.listen(8000);
