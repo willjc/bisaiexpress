64612b56f69dd1dc7227c1a60bae5f12f7542357
wangwen <8863704@qq.com>
2018-01-24 21:30:58 +0800

    动态路由以及get传值

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express=require('express');
 
 var app = new express();
+//这都是动态路由
 app.get('/',function(req,res){
-    res.send('this is / page');
+    res.send('this is a page');
 });
-app.get('/login',function(req,res){
-    res.send('thisi is login');
+app.get('/login/:aid/:pid',function(req,res){
+    //console.log(req.params['aid']);
+    var a= req.params;
+    res.send(a.aid+"<br>"+a.pid);
 
+});
+app.get('/product',function(req,res){
+
+        res.send('aid是'+req.query.aid+"<br>"+"pid是"+req.query.pid);
 });
 app.listen(8000);
